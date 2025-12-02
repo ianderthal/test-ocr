c022fa67897e146d5dcdb6b6279f9d14deb81bb3
Ian Gallardo <ian@iangallardo.com>
2025-12-01 20:11:09 -0500

    test actual image

--- ocr_test.py
+++ ocr_test.py
@@ -1,7 +1,7 @@
 import cv2
 import pytesseract
 
-img = cv2.imread('test_image.png')
+img = cv2.imread('test_image_2.jpg')
 custom_config = r'--oem 3 --psm 6'
 text = pytesseract.image_to_string(img, config=custom_config)
 
